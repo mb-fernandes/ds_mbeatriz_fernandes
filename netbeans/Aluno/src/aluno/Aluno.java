9efe30281c776ffb87814adf0c42cd3275519191
Maria Beatriz <fernandeslopes16@hotmail.com>
2024-05-13 11:56:56 -0300

    aula sobre netbeans

---
+++ netbeans/Aluno/src/aluno/Aluno.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aluno;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Aluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AlunoObj aluno1;
+        
+        aluno1 = new AlunoObj();
+        
+        aluno1.setNome("Mabi");
+        System.out.println(aluno1.getNome());
+        
+    }
+    
+}
